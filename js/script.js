8059406b9e48079c7daf41d5109ffc5f7b563569
Leonam da Silva Santana <62152870+leehsanttana@users.noreply.github.com>
2021-08-16 14:26:38 -0300

    Acrescentando as regras de negócio e corrigindo erros referente aos call to actions

--- js/script.js
+++ js/script.js
@@ -11,12 +11,28 @@ const modal = document.querySelector('.modal');
 const spanMsg = document.querySelector('.mensage');
 const spanImc = document.querySelector('.imc');
 
-function calcularIMC(event){
-
+function calcularIMC(){
 
   const pesoValor = parseFloat(inputPeso.value);
   const alturaValor = parseFloat(inputAltura.value.replace(',', '.'));
   const valorTotal = alturaValor*alturaValor;
+
+  modal.classList.remove('active');
+
+  modal.classList.remove('active');
+  modal.classList.remove('abaixo-peso');
+  modal.classList.remove('peso-normal');
+  modal.classList.remove('sobrepeso');
+  modal.classList.remove('obesidade-i');
+  modal.classList.remove('obesidade-ii');
+  modal.classList.remove('obesidade-iii');
+
+  levels[0].classList.remove('abaixo-peso');
+  levels[1].classList.remove('peso-normal');
+  levels[2].classList.remove('sobrepeso');
+  levels[3].classList.remove('obesidade-i');
+  levels[4].classList.remove('obesidade-ii');
+  levels[5].classList.remove('obesidade-iii');
 
   if (inputPeso.value === '') {
     msgError[0].classList.add('active');
@@ -31,37 +47,37 @@ function calcularIMC(event){
     if (imc <= 18.5) {
       levels[0].classList.add('abaixo-peso');
       modal.classList.add('abaixo-peso');
-      spanMsg.innerHTML = 'Cuidado! '
+      spanMsg.innerHTML = 'Cuidado! 🤨'
       spanImc.innerHTML = imc;
     
     } else if (imc >= 18.6 && imc <= 24.9) {
       levels[1].classList.add('peso-normal');
       modal.classList.add('peso-normal');
-      spanMsg.innerHTML = 'Parabéns! '
-      spanImc.innerHTML = imc;
+      spanMsg.innerHTML = 'Parabéns! 👏'
+      spanImc.innerHTML = imc ;
 
     } else if (imc >= 25 && imc <= 29.9) {
       levels[2].classList.add('sobrepeso');
       modal.classList.add('sobrepeso');
-      spanMsg.innerHTML = 'Cuidado! '
+      spanMsg.innerHTML = 'Tudo bem. 😐'
       spanImc.innerHTML = imc;
 
     } else if (imc >= 30 && imc <= 34.9) {
       levels[3].classList.add('obesidade-i');
       modal.classList.add('obesidade-i');
-      spanMsg.innerHTML = 'Olha a boca! '
+      spanMsg.innerHTML = 'Atenção! 😦'
       spanImc.innerHTML = imc;
       
     } else if (imc >= 35 && imc <= 39.9) {
       levels[4].classList.add('obesidade-ii');
       modal.classList.add('obesidade-ii');
-      spanMsg.innerHTML = 'Atenção! '
+      spanMsg.innerHTML = 'Alerta! 😨'
       spanImc.innerHTML = imc;
 
     } else if (imc >= 40) {
       levels[5].classList.add('obesidade-iii');
       modal.classList.add('obesidade-iii');
-      spanMsg.innerHTML = 'Alerta!!! '
+      spanMsg.innerHTML = 'Procure um médico!!! 😱'
       spanImc.innerHTML = imc;
       
     }
@@ -101,6 +117,12 @@ function cleanValues(event){
   event.preventDefault();
 
   modal.classList.remove('active');
+  modal.classList.remove('abaixo-peso');
+  modal.classList.remove('peso-normal');
+  modal.classList.remove('sobrepeso');
+  modal.classList.remove('obesidade-i');
+  modal.classList.remove('obesidade-ii');
+  modal.classList.remove('obesidade-iii');
 
   inputAltura.value = '';
   inputPeso.value = '';
